9a27b69cf220e777d03b2c7a5759bfdd592ba447
sudmale <sudmale@xento.com>
2021-05-20 13:23:17 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
 // sachin committing
+// new
 ReactDOM.render(<App key='test' />, document.getElementById("root"));
