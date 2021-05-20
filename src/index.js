139f4d3ed8cf72422e5487e871a5b3887482453f
sudmale <sudmale@xento.com>
2021-05-20 13:19:09 +0530

    su first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
-
-
+// sachin committing
 ReactDOM.render(<App key='test' />, document.getElementById("root"));
